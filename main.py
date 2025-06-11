fac1f973484b1e6234fe8fcd811a87b46f285894
Michael Nöbl <michaelnoebl@gmail.com>
2025-06-11 17:12:39 +0200

    CHORE: outsource 'main'-method

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from temperature_monitor import TemperatureMonitor
 from dotenv import load_dotenv
 import os
 
-if __name__ == "__main__":
+def main():
     load_dotenv()
 
-    WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
-    PUSHOVER_TOKEN = os.getenv("PUSHOVER_TOKEN")
-    PUSHOVER_USER = os.getenv("PUSHOVER_USER")
+    weather_api_key = os.getenv("weather_api_key")
+    pushover_token = os.getenv("pushover_token")
+    pushover_user = os.getenv("pushover_user")
 
-    monitor = TemperatureMonitor(api_key=WEATHER_API_KEY, pushover_token=PUSHOVER_TOKEN, pushover_user=PUSHOVER_USER)
+    monitor = TemperatureMonitor(api_key=weather_api_key, pushover_token=pushover_token, pushover_user=pushover_user)
     monitor.run()
+
+if __name__ == "__main__":
+    main()
