9a3549894b2742344f156e3c4bf8828502b3f751
Michael Nöbl <michaelnoebl@gmail.com>
2025-06-11 17:15:42 +0200

    CHORE: capitalize env-variables

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import os
 def main():
     load_dotenv()
 
-    weather_api_key = os.getenv("weather_api_key")
-    pushover_token = os.getenv("pushover_token")
-    pushover_user = os.getenv("pushover_user")
+    weather_api_key = os.getenv("WEATHER_API_KEY")
+    pushover_token = os.getenv("PUSHOVER_TOKEN")
+    pushover_user = os.getenv("PUSHOVER_USER")
 
     monitor = TemperatureMonitor(api_key=weather_api_key, pushover_token=pushover_token, pushover_user=pushover_user)
     monitor.run()
